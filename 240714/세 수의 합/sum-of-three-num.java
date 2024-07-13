ab437e15661d9ec7c0bf77299c9536572d88d229
20s_hoon <39319854+98-jeonghoon@users.noreply.github.com>
2024-07-14 08:53:01 +0900

    Title: 세 수의 합, Time: 217ms, Memory: 18MB, Status: Passed - CodeTree

--- 240714/세 수의 합/sum-of-three-num.java
+++ 240714/세 수의 합/sum-of-three-num.java
@@ -1,40 +1,42 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.*;
-
 public class Main {
     static int n, k;
     static int answer = 0;
-
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
 
         n = Integer.parseInt(st.nextToken());
         k = Integer.parseInt(st.nextToken());
 
         int[] arr = new int[n];
+
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-        
-        HashMap<Integer, Integer> hashMap = new HashMap<>();
-
-        for (int i = 0; i < n - 2; i++) {
-            for (int j = i + 1; j < n; j++) {
-                int sumOfTwo = arr[i] + arr[j];
-                hashMap.put(sumOfTwo, hashMap.getOrDefault(sumOfTwo, 0) + 1);
-            }
-        }
 
         for (int i = 0; i < n; i++) {
-            int target = k - arr[i];
-            if (hashMap.containsKey(target)) {
-                answer += hashMap.get(target);
+            for (int j = i + 1; j < n; j++) {
+                int first = arr[i];
+                int second = arr[j];
+                int complement = k - first - second;
+
+                if (hashMap.containsKey(complement)) {
+                    answer += hashMap.get(complement);
+                }
+
+            }
+            if (hashMap.containsKey(arr[i])) {
+                hashMap.put(arr[i], hashMap.get(arr[i]) + 1);
+            } else {
+                hashMap.put(arr[i], 1);
             }
         }
+        System.out.println(answer);
 
-        System.out.println(answer / 3);
     }
 }
