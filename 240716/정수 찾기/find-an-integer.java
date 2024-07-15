bf334a1dcf158f960a0bfdeafabb59ece70267ec
20s_hoon <39319854+98-jeonghoon@users.noreply.github.com>
2024-07-16 07:51:50 +0900

    Title: 정수 찾기, Time: 942ms, Memory: 37MB, Status: Passed - CodeTree

---
+++ 240716/정수 찾기/find-an-integer.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int n, m;
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+
+        HashSet<Integer> hashSet = new HashSet<>();
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++){
+            hashSet.add(Integer.parseInt(st.nextToken()));
+        }
+
+        st = new StringTokenizer(br.readLine());
+        m = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < m; i++) {
+            if (hashSet.contains(Integer.parseInt(st.nextToken()))) {
+                System.out.println(1);
+            } else {
+                System.out.println(0);
+            }
+        }
+
+    }
+}
