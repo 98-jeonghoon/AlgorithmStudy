5ae5d6b4a712e36a718ad32ba3c6649fc9c5b4ee
20s_hoon <39319854+98-jeonghoon@users.noreply.github.com>
2024-07-20 07:14:30 +0900

    Title: 최대로 연속한 숫자, Time: 1525ms, Memory: 41MB, Status: Passed - CodeTree

--- 240720/최대로 연속한 숫자/maximum-consecutive-number.java
+++ 240720/최대로 연속한 숫자/maximum-consecutive-number.java
@@ -1,44 +1,65 @@
 import java.util.*;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        TreeSet<Integer> set = new TreeSet<>();
-        for (int i = 0; i <= n; i++) {
-            set.add(i);
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        TreeSet<Integer> s_num = new TreeSet<>();
+        TreeSet<Tuple> s_len = new TreeSet<>();
+
+        // 초기 값 설정
+        s_num.add(-1);
+        s_num.add(n + 1);
+        s_len.add(new Tuple(n + 1, -1, n + 1));
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < m; i++) {
+
+            int y = Integer.parseInt(st.nextToken());
+
+            // y의 바로 뒤와 바로 앞의 숫자를 찾음
+            int z = s_num.higher(y);
+            int x = s_num.lower(y);
+
+            // s_num에 y를 추가
+            s_num.add(y);
+
+            // 기존 구간 (x, z) 제거 및 새로운 구간 추가
+            s_len.remove(new Tuple(z - x - 1, x, z));
+            s_len.add(new Tuple(y - x - 1, x, y));
+            s_len.add(new Tuple(z - y - 1, y, z));
+
+            // 가장 긴 구간의 길이 출력
+            Tuple best = s_len.last();
+            System.out.println(best.length);
+        }
+    }
+
+    // Tuple 클래스 정의
+    static class Tuple implements Comparable<Tuple> {
+        int length, start, end;
+
+        Tuple(int length, int start, int end) {
+            this.length = length;
+            this.start = start;
+            this.end = end;
         }
 
-        int[] toRemove = new int[m];
-        for (int i = 0; i < m; i++) {
-            toRemove[i] = sc.nextInt();
-        }
-
-        for (int i = 0; i < m; i++) {
-            int num = toRemove[i];
-
-            // 현재 숫자 제거
-            set.remove(num);
-
-            // 현재 상태에서 가장 긴 연속된 부분 수열의 길이 계산
-            int maxLen = 0;
-            int currentLen = 0;
-            Integer prev = null;
-            for (Integer val : set) {
-                if (prev == null || val == prev + 1) {
-                    currentLen++;
-                } else {
-                    maxLen = Math.max(maxLen, currentLen);
-                    currentLen = 1;
-                }
-                prev = val;
+        @Override
+        public int compareTo(Tuple other) {
+            if (this.length != other.length) {
+                return Integer.compare(this.length, other.length);
+            } else if (this.start != other.start) {
+                return Integer.compare(this.start, other.start);
+            } else {
+                return Integer.compare(this.end, other.end);
             }
-            maxLen = Math.max(maxLen, currentLen); // 마지막 구간 체크
-
-            // 결과 출력
-            System.out.println(maxLen);
         }
+
     }
 }
