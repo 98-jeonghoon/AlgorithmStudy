1a67ff55671bbc76188e1c7306ba2af228959943
20s_hoon <39319854+98-jeonghoon@users.noreply.github.com>
2024-07-20 08:16:12 +0900

    Title: 길이가 무한대인 수평선 트랙, Time: 127ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240720/길이가 무한대인 수평선 트랙/horizontal-track-with-infinite-length.java
+++ 240720/길이가 무한대인 수평선 트랙/horizontal-track-with-infinite-length.java
@@ -3,28 +3,28 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+
         int N = sc.nextInt();
         int T = sc.nextInt();
-        
-        long[] finalPositions = new long[N];
-        
+
+        List<Long> finalPositions = new ArrayList<>();
+
         for (int i = 0; i < N; i++) {
             long start = sc.nextLong();
             long speed = sc.nextLong();
-            finalPositions[i] = start + speed * T;
+            finalPositions.add(start + speed * T);
         }
-        
-        Arrays.sort(finalPositions);
-        
-        int groups = 0;
-        
-        for (int i = 1; i < N; i++) {
-            if (finalPositions[i] != finalPositions[i - 1]) {
-                groups++;
+
+        TreeSet<Long> groups = new TreeSet<>();
+
+        for (long position : finalPositions) {
+            Long higher = groups.ceiling(position);
+            if (higher != null) {
+                groups.remove(higher);
             }
+            groups.add(position);
         }
-        
-        System.out.println(groups);
+
+        System.out.println(groups.size());
     }
 }
