57d729689cf0073bb7aa5b0f7a1b229d8102c46a
20s_hoon <39319854+98-jeonghoon@users.noreply.github.com>
2024-07-23 02:50:59 +0900

    Title: 배열 추출, Time: 1692ms, Memory: 62MB, Status: Passed - CodeTree

---
+++ 240723/배열 추출/array-extraction.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            int value = sc.nextInt();
+            if (value == 0) {
+                if (pq.isEmpty()) {
+                    System.out.println(0);
+                } else {
+                    System.out.println(pq.poll());
+                }
+            } else {
+                pq.add(value);
+            }
+        }
+    }
+}
