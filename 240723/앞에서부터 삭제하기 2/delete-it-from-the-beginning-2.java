2322e978b08324dfd61f055a8da11ccde098f6b6
20s_hoon <39319854+98-jeonghoon@users.noreply.github.com>
2024-07-23 04:02:55 +0900

    Title: 앞에서부터 삭제하기 2, Time: 877ms, Memory: 34MB, Status: Passed - CodeTree

--- 240723/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.java
+++ 240723/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.java
@@ -1,44 +1,43 @@
 import java.util.Scanner;
+import java.util.PriorityQueue;
 
 public class Main {
+    public static final int MAX_N = 100000;
+    
+    // 변수 선언
+    public static int n;
+    public static int[] arr = new int[MAX_N];
+    public static int sumVal;
+    public static double maxAvg;
+    public static PriorityQueue<Integer> pq = new PriorityQueue<>();  
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
+        // 입력:
+        n = sc.nextInt();
+        for(int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
 
-        int N = scanner.nextInt();
-        int[] arr = new int[N + 1];
+        pq.add(arr[n - 1]);
+        sumVal += arr[n - 1];
+        // k가 N - 2일 때부터 1일 때까지 거꾸로 탐색합니다.
+        // priority queue를 이용하여 진행합니다.
+        for(int i = n - 2; i >= 1; i--) {
+            // 앞에서부터 K개를 삭제하고 나면
+            // 뒤에 i ~ n - 1 까지의 숫자만이 남습니다.
+            pq.add(arr[i]);
+            sumVal += arr[i];
 
-        for (int i = 1; i <= N; i++) {
-            arr[i] = scanner.nextInt();
+            // 남아있는 정수 중 가장 작은 숫자를 찾아
+            // 그 숫자를 제외한 평균을 구합니다.
+            double avg = (double)(sumVal - pq.peek()) / (n - i - 1);
+
+            // 평균이 최대가 된다면 정답을 현재 평균으로 갱신해줍니다.
+            if(maxAvg < avg)
+                maxAvg = avg;
         }
 
-        System.out.printf("%.2f\n", getMaxAverageAfterDeletions(N, arr));
-    }
-
-    public static double getMaxAverageAfterDeletions(int N, int[] arr) {
-        int[] prefix = new int[N + 1];
-        for (int i = 1; i <= N; i++) {
-            prefix[i] = prefix[i - 1] + arr[i];
-        }
-        
-        int[] postfix = new int[N + 1];
-        int temp = 10001;
-        for (int i = N; i >= 0; i--) {
-            postfix[i] = Math.min(temp, arr[i]);
-            temp = postfix[i];
-        }
-
-        int total = 0;
-        for (int i = 1; i <= N; i++) {
-            total += arr[i];
-        }
-
-        double ans = 0;
-        for (int k = 1; k < N - 1; k++) {
-            int summation = total - prefix[k] - postfix[k + 1];
-            double avg = (double) summation / (N - k - 1);
-            ans = Math.max(ans, avg);
-        }
-
-        return ans;
+        // 평균값의 최대를 출력합니다.
+        System.out.printf("%.2f", maxAvg);
     }
 }
