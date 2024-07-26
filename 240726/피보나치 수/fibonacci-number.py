16fbe922fa8bc589ff025a09062f098e785215cf
20s_hoon <39319854+98-jeonghoon@users.noreply.github.com>
2024-07-26 21:57:40 +0900

    Title: 피보나치 수, Time: 145ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240726/피보나치 수/fibonacci-number.py
@@ -0,0 +1,10 @@
+def fibonacci(n):
+    if n == 1 or n == 2:
+        return 1
+    a, b = 1, 1
+    for _ in range(n - 2):
+        a, b = b, a + b
+    return b
+
+N = int(input())
+print(fibonacci(N))
