df6fe7c9bce6827971efd17d49e8dbb31097cb89
20s_hoon <39319854+98-jeonghoon@users.noreply.github.com>
2024-07-27 23:13:16 +0900

    Title: 계단 오르기, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240727/계단 오르기/climbing-stairs.py
@@ -0,0 +1,14 @@
+n = int(input())
+dp = [0] * (10001)
+dp[0] = 0
+dp[1] = 0
+dp[2] = 1
+dp[3] = 1
+
+if n <= 3:
+    print(dp[n])
+    exit(0)
+for i in range(4, n + 1):
+    dp[i] = dp[i - 2] + dp[i - 3]
+
+print(dp[n])
