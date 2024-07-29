606cafa90dbf2ef4fa66ded1a7fcd90cdf7d1cb2
20s_hoon <39319854+98-jeonghoon@users.noreply.github.com>
2024-07-29 20:34:13 +0900

    Title: 사각형 채우기 3, Time: 136ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240729/사각형 채우기 3/rectangle-fill-3.java
+++ 240729/사각형 채우기 3/rectangle-fill-3.java
@@ -6,20 +6,18 @@ public class Main {
         int n = scanner.nextInt();
         scanner.close();
         
-        long[] dp = new long[1001];
-        long[] dphalf = new long[1001];
-        int MOD = 1000000007;
+        long[] dp = new long[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
 
-        dp[1] = 2;
-        dp[2] = 7;
-        dphalf[1] = 1;
-        dphalf[2] = 3;
-        
-        for (int i = 3; i <= n; i++) {
-            dp[i] = (dp[i-1]*2 + dp[i-2] + dphalf[i-1] * 2) % MOD;
-            dphalf[i] = (dp[i-1] + dphalf[i-1]) % MOD;
+        for (int i = 2; i <= n; i++) {
+            long sum = 0;
+            for (int j = 0; j < i; j++) {
+                sum += dp[j] * dp[i - j - 1];
+            }
+            dp[i] = sum;
         }
-        
+
         System.out.println(dp[n]);
     }
 }
