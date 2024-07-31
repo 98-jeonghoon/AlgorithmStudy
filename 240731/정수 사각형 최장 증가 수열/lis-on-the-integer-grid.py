16f65fd5d6775fc1eb38f0d454f87bb326e0bb74
20s_hoon <39319854+98-jeonghoon@users.noreply.github.com>
2024-07-31 16:31:47 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 754ms, Memory: 54MB, Status: Passed - CodeTree

--- 240731/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
+++ 240731/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
@@ -1,34 +1,29 @@
-import sys
-sys.setrecursionlimit(100000)
-
-
-def dfs(x, y):
-    if dp[x][y] != -1:
-        return dp[x][y]
-
-    dp[x][y] = 1
-
-    for d in range(4):
-        nx = x + dx[d]
-        ny = y + dy[d]
-
-        if 0 <= nx < n and 0 <= ny < n and graph[nx][ny] > graph[x][y]:
-            dp[x][y] = max(dp[x][y], dfs(nx, ny) + 1)
-
-    return dp[x][y]
-
-
 n = int(input())
 graph = [list(map(int, input().split())) for _ in range(n)]
+dp = [[1] * n for _ in range(n)]
 
-dp = [[-1] * n for _ in range(n)]
+pos = []
+
+for x in range(n):
+    for y in range(n):
+        pos.append((graph[x][y], x, y))
+
+pos.sort()
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-result = 0
-for i in range(n):
-    for j in range(n):
-        result = max(result, dfs(i, j))
+for _, x, y in pos:
+    for d in range(4):
+        nx = x + dx[d]
+        ny = y + dy[d]
+        if 0 <= nx < n and 0 <= ny < n and graph[nx][ny] > graph[x][y]:
+            dp[nx][ny] = max(dp[nx][ny], dp[x][y] + 1)
 
-print(result)
+answer = 0
+
+for x in range(n):
+    for y in range(n):
+        answer = max(answer, dp[x][y])
+
+print(answer)
