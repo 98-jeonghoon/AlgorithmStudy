db778f5cdc224c015a6d673fdbd909c8c7fbbe9d
20s_hoon <39319854+98-jeonghoon@users.noreply.github.com>
2024-08-03 03:52:01 +0900

    Title: 동전 거슬러주기, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240803/동전 거슬러주기/coin-change.py
+++ 240803/동전 거슬러주기/coin-change.py
@@ -1,16 +1,16 @@
 n, m = map(int, input().split())
 coins = list(map(int, input().split()))
 
-dp = [-1e9] * (m + 1)
-dp = [0]
+dp = [1e9] * (m + 1)
+dp[0] = 0
 
 for i in range(1, m + 1):
     for j in range(n):
         if i >= coins[j]:
-            if dp[i - coins[j]] == -1e9:
+            if dp[i - coins[j]] == 1e9:
                 continue
-            
-            dp[i] = max(dp[i], dp[i - coins[j]] + 1)
+
+            dp[i] = min(dp[i], dp[i - coins[j]] + 1)
 
 answer = dp[m]
 if answer == -1e9:
