56510aa2520277c89cd7248e71e47e11cc32de90
20s_hoon <39319854+98-jeonghoon@users.noreply.github.com>
2024-08-03 02:43:43 +0900

    Title: 최대 감소 부분 수열, Time: 156ms, Memory: 25MB, Status: Passed - CodeTree

--- 240803/최대 감소 부분 수열/longest-decreasing-subsequence.py
+++ 240803/최대 감소 부분 수열/longest-decreasing-subsequence.py
@@ -3,7 +3,8 @@ arr = list(map(int, input().split()))
 
 dp = [0] * (n + 1)
 
-for i in range(1, n):
+for i in range(n):
+    dp[i] = 1
     for j in range(i):
         if arr[i] < arr[j]:
             dp[i] = max(dp[i], dp[j] + 1)
