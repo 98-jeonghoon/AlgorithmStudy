5549bc7211c7af4cb21156d630517c90c48030b7
20s_hoon <39319854+98-jeonghoon@users.noreply.github.com>
2024-08-02 16:55:10 +0900

    Title: 겹치지 않게 선분 고르기 2, Time: 153ms, Memory: 25MB, Status: Passed - CodeTree

--- 240802/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.py
+++ 240802/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.py
@@ -1,16 +1,13 @@
 def max_non_overlapping_segments(n, segments):
-    segments.sort(key=lambda x: x[1])
-
-    count = 0
-    last_end = -1
-
-    for start, end in segments:
-        # 현재 선분의 시작점이 이전 선분의 끝점보다 크면 선택
-        if start > last_end:
-            count += 1
-            last_end = end
-
-    return count
+    segments.sort(key=lambda x: x[0])
+    dp = [1] * n
+    
+    for i in range(1, n):
+        for j in range(i):
+            if segments[i][0] > segments[j][1]:
+                dp[i] = max(dp[i], dp[j] + 1)
+    
+    return max(dp)
 
 
 n = int(input())
@@ -18,6 +15,7 @@ segments = []
 for _ in range(n):
     x1, x2 = map(int, input().split())
     segments.append((x1, x2))
+
 
 result = max_non_overlapping_segments(n, segments)
 print(result)
