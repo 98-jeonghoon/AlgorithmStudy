8c2af078b816c56385b6e7207b2d8b2a065430dc
20s_hoon <39319854+98-jeonghoon@users.noreply.github.com>
2024-08-03 02:52:45 +0900

    Title: 증가했다가 감소하는 부분 수열, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240803/증가했다가 감소하는 부분 수열/increasing-and-descreasing-subsequence.py
+++ 240803/증가했다가 감소하는 부분 수열/increasing-and-descreasing-subsequence.py
@@ -8,15 +8,15 @@ def dp_logic(arr):
     lis = [1] * n
     for i in range(1, n):
         for j in range(i):
-            if arr[i] > arr[j] and lis[i] < lis[j] + 1:
-                lis[i] = lis[j] + 1
+            if arr[i] > arr[j]:
+                lis[i] = max(lis[i], lis[j] + 1)
     
-    # LDS 계산 (뒤에서부터)
+    # LDS 계산 (앞에서부터)
     lds = [1] * n
-    for i in range(n-2, -1, -1):
-        for j in range(n-1, i, -1):
-            if arr[i] > arr[j] and lds[i] < lds[j] + 1:
-                lds[i] = lds[j] + 1
+    for i in range(1, n):
+        for j in range(i):
+            if arr[i] < arr[j]:
+                lds[i] = max(lds[i], lds[j] + 1)
     
     # 최장 증가-감소 부분 수열 길이 계산
     max_length = 0
