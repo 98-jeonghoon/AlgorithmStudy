22af124fdf8615952be3a35d290fed93f6a76983
20s_hoon <39319854+98-jeonghoon@users.noreply.github.com>
2024-08-03 00:41:33 +0900

    Title: 알바로 부자 되기, Time: 153ms, Memory: 25MB, Status: Passed - CodeTree

--- 240803/알바로 부자 되기/being-rich-by-working-part-time.py
+++ 240803/알바로 부자 되기/being-rich-by-working-part-time.py
@@ -6,14 +6,11 @@ dp = [0] * n
 for i in range(n):
     s, e, p = map(int, input().split())
     arr.append((s, e, p))
-    dp[i] = p
 
-arr.sort(key = lambda x:(x[0], x[1], -x[2]))
-
-for i in range(1, n):
+for i in range(n):
+    dp[i] = arr[i][2]  # 현재 작업만 선택하는 경우
     for j in range(i):
-        if arr[i][0] > arr[j][1]:
-            dp[i] = max(dp[i], dp[j] + arr[j][2])
+        if arr[i][0] > arr[j][1]:  # 겹치지 않는 경우
+            dp[i] = max(dp[i], dp[j] + arr[i][2])
 
 print(max(dp))
-# print(arr)
