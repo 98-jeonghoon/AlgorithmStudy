bc811bbbd47d489dea5b6bd9c3fbe0050f2b84b2
20s_hoon <39319854+98-jeonghoon@users.noreply.github.com>
2024-08-05 00:33:33 +0900

    Title: 개발자의 순위, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240805/개발자의 순위/developer's-rank.cpp
+++ 240805/개발자의 순위/developer's-rank.cpp
@@ -1,31 +1,38 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-int k, n;
-
 int main() {
+    int k, n;
     cin >> k >> n;
-    int arr[100][100];
-    for(int i = 0; i < k; i++){
-        for(int j = 0; j < n; j++){
-            cin >> arr[i][j];
+    
+    vector<vector<int>> rank(k, vector<int>(n + 1));
+    
+    for (int i = 0; i < k; i++) {
+        for (int j = 0; j < n; j++) {
+            int developer;
+            cin >> developer;
+            rank[i][developer] = j;  // 각 개발자의 순위를 기록
         }
     }
     
-    int total_cnt = 0;
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
-            if(j == i) continue;
-            int cnt = 0;
+    int count = 0;
+    for (int a = 1; a <= n; a++) {
+        for (int b = 1; b <= n; b++) {
+            if (a == b) continue;
             
-            for(int l = 0; l < k; l++){
-                if(arr[l][i] > arr[l][j]) ++cnt;
+            bool always_better = true;
+            for (int game = 0; game < k; game++) {
+                if (rank[game][a] >= rank[game][b]) {
+                    always_better = false;
+                    break;
+                }
             }
             
-            if(cnt == k) ++total_cnt;
-        }   
+            if (always_better) count++;
+        }
     }
     
-    cout << total_cnt;
+    cout << count << endl;
     return 0;
 }
