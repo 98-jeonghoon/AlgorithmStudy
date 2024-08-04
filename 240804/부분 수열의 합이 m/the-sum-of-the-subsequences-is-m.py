21545a64170b38eaed4edf71fdcaaa32371c0d35
20s_hoon <39319854+98-jeonghoon@users.noreply.github.com>
2024-08-04 19:02:06 +0900

    Title: 부분 수열의 합이 m, Time: 146ms, Memory: 25MB, Status: Passed - CodeTree

--- 240804/부분 수열의 합이 m/the-sum-of-the-subsequences-is-m.py
+++ 240804/부분 수열의 합이 m/the-sum-of-the-subsequences-is-m.py
@@ -1,14 +1,14 @@
 n, m = map(int, input().split())
 arr = list(map(int, input().split()))
 
-from itertools import combinations
+dp = [1e9] * (m + 1)
+dp[0] = 0
 
-combs = []
-for i in range(n):
-    for comb in combinations(arr, i):
-        combs.append(comb)
-        if sum(comb) == m:
-            print(len(comb))
-            exit(0)
+for num in arr:
+    for i in range(m, num - 1, -1):
+        dp[i] = min(dp[i], dp[i - num] + 1)
+
+if dp[m] != 1e9:
+    print(dp[m])
 else:
     print(-1)
