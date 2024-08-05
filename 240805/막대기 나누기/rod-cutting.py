9bc0f624851852cb8de830872338203bcc52fd60
20s_hoon <39319854+98-jeonghoon@users.noreply.github.com>
2024-08-05 14:53:21 +0900

    Title: 막대기 나누기, Time: 244ms, Memory: 25MB, Status: Passed - CodeTree

--- 240805/막대기 나누기/rod-cutting.py
+++ 240805/막대기 나누기/rod-cutting.py
@@ -1,25 +1,10 @@
 n = int(input())
-sticks = [(0, 0)]
-arr = list(map(int, input().split()))
+prices = [0] + list(map(int, input().split()))
+
+dp = [0] * (n + 1)
+
 for i in range(1, n + 1):
-    sticks.append((i, arr[i - 1]))
+    for j in range(1, i + 1):
+        dp[i] = max(dp[i], dp[i - j] + prices[j])
 
-
-dp = [[0] * (n + 1) for _ in range(n + 1)]
-
-# 길이 먼저 시작
-for length in range(1, n + 1):
-    for stick in range(1, n + 1):
-        stick_len, stick_value = sticks[stick]
-        #  선택된 길이로 현재 길이를 만들 수 없다면
-        if stick_len > length:
-            # 전에 것 그대로 가져옴
-            dp[length][stick] = dp[length][stick - 1]
-        else:
-            if length % stick_len == 0:
-                quotient = length // stick_len
-                dp[length][stick] = max(dp[length][stick - 1], stick_value * quotient)
-                continue
-            dp[length][stick] = max(dp[length][stick - 1], dp[length - stick_len][stick - 1] + stick_value)
-
-print(dp[n][n])
+print(dp[n])
