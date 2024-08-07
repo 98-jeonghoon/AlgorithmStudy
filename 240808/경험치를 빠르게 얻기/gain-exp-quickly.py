7c85e66dc4049c0a576e5bf12e47fb426ac27321
20s_hoon <39319854+98-jeonghoon@users.noreply.github.com>
2024-08-08 03:26:26 +0900

    Title: 경험치를 빠르게 얻기, Time: 1445ms, Memory: 40MB, Status: Passed - CodeTree

--- 240808/경험치를 빠르게 얻기/gain-exp-quickly.py
+++ 240808/경험치를 빠르게 얻기/gain-exp-quickly.py
@@ -3,16 +3,21 @@ n, m = map(int, input().split())
 
 quests = [tuple(map(int, input().split())) for _ in range(n)]
 
-INF = float('inf')
-max_exp = sum(e for e, t in quests) 
-dp = [INF] * (max_exp + 1)
-dp[0] = 0  
 
+max_exp = sum(e for e, t in quests)
+
+INF = float('inf')
+dp = [INF] * (max_exp + 1)
+dp[0] = 0 
 for e, t in quests:
     for j in range(max_exp, e - 1, -1):
         if dp[j - e] != INF:
             dp[j] = min(dp[j], dp[j - e] + t)
-min_time = min(dp[m:])
+
+min_time = INF
+for j in range(m, max_exp + 1):
+    if dp[j] < min_time:
+        min_time = dp[j]
 
 if min_time == INF:
     print(-1)
