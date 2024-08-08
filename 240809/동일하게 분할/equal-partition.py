a6ef8564aa997e26beb45147f2a31635408316de
20s_hoon <39319854+98-jeonghoon@users.noreply.github.com>
2024-08-09 00:31:29 +0900

    Title: 동일하게 분할, Time: 183ms, Memory: 25MB, Status: Passed - CodeTree

--- 240809/동일하게 분할/equal-partition.py
+++ 240809/동일하게 분할/equal-partition.py
@@ -2,22 +2,22 @@ n = int(input())
 numbers = list(map(int, input().split()))
 
 total_sum = sum(numbers)
-dp = [False] * (total_sum + 1)
-dp[0] = True
 
-for num in numbers:
-    for j in range(total_sum, num - 1, -1):
-        if dp[j - num]:
-            dp[j] = True
-
-answer = False
-for i in range(1, total_sum + 1):
-    if dp[i] == True:
-        if total_sum - 2 * i == 0:
-            answer = True
-            break
-
-if answer:
-    print("Yes")
-else:
+# 두 그룹으로 나눌 수 있는지 확인
+if total_sum % 2 != 0:
     print("No")
+else:
+    target = total_sum // 2
+    dp = [False] * (target + 1)
+    dp[0] = True
+    
+    for num in numbers:
+        for j in range(target, num - 1, -1):
+            if dp[j - num]:
+                dp[j] = True
+
+    # 적어도 하나의 요소를 포함하는 두 그룹으로 나눌 수 있는지 확인
+    if dp[target] and any(dp[target - num] for num in numbers):
+        print("Yes")
+    else:
+        print("No")
