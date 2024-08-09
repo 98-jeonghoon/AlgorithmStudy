1e8007e452007d9aa19aed3a72fc93996a5af189
20s_hoon <39319854+98-jeonghoon@users.noreply.github.com>
2024-08-09 16:35:24 +0900

    Title: 계단 오르기 2, Time: 119ms, Memory: 25MB, Status: Passed - CodeTree

--- 240809/계단 오르기 2/climbing-stairs-2.py
+++ 240809/계단 오르기 2/climbing-stairs-2.py
@@ -1,17 +1,23 @@
 n = int(input())
 coins = [0] + list(map(int, input().split()))
 
-dp = [[0] * 4 for _ in range(n + 1)]
+# dp[i][j]는 i층에 도착했을 때, 1칸을 j번 사용하여 얻을 수 있는 최대 동전의 개수
+dp = [[-1] * 4 for _ in range(n + 1)]
 
-dp[1][1] = coins[1]
+# 초기 조건 설정
+dp[1][1] = coins[1]  # 첫 번째 층에 1칸으로 올라갔을 경우
+dp[2][0] = coins[2]  # 두 번째 층에 2칸으로 올라갔을 경우
 
-for i in range(2, n + 1):
+for i in range(3, n + 1):
     for j in range(4):
-        if j > 0:
-            # 1칸 올라가는 경우
-            dp[i][j] = max(dp[i][j], dp[i - 1][j - 1] + coins[i])
-        if i >= 2:
-            # 2칸 올라가는 경우
-            dp[i][j] = max(dp[i][j], dp[i - 2][j] + coins[i])
+        # 1칸 올라가는 경우 (최대 3번까지)
+        if j > 0 and dp[i-1][j-1] != -1:
+            dp[i][j] = max(dp[i][j], dp[i-1][j-1] + coins[i])
+        
+        # 2칸 올라가는 경우
+        if dp[i-2][j] != -1:
+            dp[i][j] = max(dp[i][j], dp[i-2][j] + coins[i])
 
-print(max(dp[n][j] for j in range(4)))
+# 마지막 층에 도달할 수 있는 경우 중 최대값 찾기
+result = max(dp[n][j] for j in range(4) if dp[n][j] != -1)
+print(result)
