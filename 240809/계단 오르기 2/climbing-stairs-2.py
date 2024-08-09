4697ff1175213a27a9e45425ad4c93fc3cbfbe9a
20s_hoon <39319854+98-jeonghoon@users.noreply.github.com>
2024-08-09 16:26:05 +0900

    Title: 계단 오르기 2, Time: 122ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240809/계단 오르기 2/climbing-stairs-2.py
@@ -0,0 +1,18 @@
+n = int(input())
+coins = [0] + list(map(int, input().split()))
+
+dp = [[0] * 4 for _ in range(n + 1)]
+
+dp[0][0] = 0
+
+# 한칸을 오를수 있는 횟수
+for j in range(1, 4):
+    # 계단 층수
+    for i in range(1, n + 1):
+        dp[i][j] = max(dp[i - 1][j - 1] + coins[i], dp[i - 2][j] + coins[i])
+
+# for i in dp:
+#     print(i)
+
+
+print(dp[-1][-1])
