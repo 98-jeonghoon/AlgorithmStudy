24ef7c986dc5318f94b83ab8494f2ad84bf3d21e
20s_hoon <39319854+98-jeonghoon@users.noreply.github.com>
2024-08-10 20:36:05 +0900

    Title: 숫자 전쟁, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240810/숫자 전쟁/number-war.py
+++ 240810/숫자 전쟁/number-war.py
@@ -1,24 +1,25 @@
-def max_score():
-    import sys
-    
-    input = sys.stdin.readline
-    
-    n = int(input().strip())
-    player1 = list(map(int, input().strip().split()))
-    player2 = list(map(int, input().strip().split()))
-
+def max_score(n, player1, player2):
+    # DP 테이블 초기화
     dp = [[0] * (n + 1) for _ in range(n + 1)]
-
-    player1.sort()
-    player2.sort()
-
-    for i in range(1, n + 1):
-        for j in range(1, n + 1):
-            if player2[j - 1] < player1[i - 1]:
-                dp[i][j] = max(dp[i - 1][j - 1] + player2[j - 1], dp[i - 1][j], dp[i][j - 1])
+    
+    # 뒤에서부터 DP 테이블 채우기
+    for i in range(n - 1, -1, -1):
+        for j in range(n - 1, -1, -1):
+            # 카드 대결
+            if player2[j] < player1[i]:
+                dp[i][j] = max(dp[i][j], dp[i][j+1] + player2[j])
             else:
-                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+                dp[i][j] = max(dp[i][j], dp[i+1][j])
+            
+            # 카드 버리기
+            dp[i][j] = max(dp[i][j], dp[i+1][j+1])
+    
+    return dp[0][0]
 
-    print(dp[n][n])
+# 입력 받기
+n = int(input())
+player1 = list(map(int, input().split()))
+player2 = list(map(int, input().split()))
 
-max_score()
+# 결과 출력
+print(max_score(n, player1, player2))
