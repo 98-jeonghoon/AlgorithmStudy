3cf797a556d5487e428b7365ef838fff5702a0c1
20s_hoon <39319854+98-jeonghoon@users.noreply.github.com>
2024-08-10 20:36:44 +0900

    Title: 숫자 전쟁, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240810/숫자 전쟁/number-war.py
+++ 240810/숫자 전쟁/number-war.py
@@ -1,25 +1,17 @@
 def max_score(n, player1, player2):
-    # DP 테이블 초기화
     dp = [[0] * (n + 1) for _ in range(n + 1)]
     
-    # 뒤에서부터 DP 테이블 채우기
     for i in range(n - 1, -1, -1):
         for j in range(n - 1, -1, -1):
-            # 카드 대결
             if player2[j] < player1[i]:
-                dp[i][j] = max(dp[i][j], dp[i][j+1] + player2[j])
+                dp[i][j] = dp[i][j+1] + player2[j]
             else:
-                dp[i][j] = max(dp[i][j], dp[i+1][j])
-            
-            # 카드 버리기
-            dp[i][j] = max(dp[i][j], dp[i+1][j+1])
+                dp[i][j] = max(dp[i+1][j], dp[i+1][j+1])
     
     return dp[0][0]
 
-# 입력 받기
 n = int(input())
 player1 = list(map(int, input().split()))
 player2 = list(map(int, input().split()))
 
-# 결과 출력
 print(max_score(n, player1, player2))
