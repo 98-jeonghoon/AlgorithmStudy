fa81ed57bbfb2dd13c116d60f1463d150b99da31
20s_hoon <39319854+98-jeonghoon@users.noreply.github.com>
2024-08-14 05:06:34 +0900

    Title: 코로나 메뉴얼, Time: 103ms, Memory: 24MB, Status: Passed - CodeTree

--- 240814/코로나 메뉴얼/covid-manual.py
+++ 240814/코로나 메뉴얼/covid-manual.py
@@ -6,25 +6,23 @@ c1, tem1 = f[0], int(f[1])
 c2, tem2 = s[0], int(s[1])
 c3, tem3 = t[0], int(t[1])
 
-def classify(cold, temp):
-    if cold == "Y":
-        if temp >= 37:
-            return "A"
-        else:
-            return "C"
-    else:
-        if temp >= 37:
-            return "B"
-        else:
-            return "D"
+# A로 분류되는 사람의 수를 저장할 변수
+a_count = 0
 
-F = classify(c1, tem1)
-S = classify(c2, tem2)
-T = classify(c3, tem3)
+# 첫 번째 사람 분류
+if c1 == "Y" and tem1 >= 37:
+    a_count += 1
 
-count_A = [F, S, T].count("A")
+# 두 번째 사람 분류
+if c2 == "Y" and tem2 >= 37:
+    a_count += 1
 
-if count_A >= 2:
+# 세 번째 사람 분류
+if c3 == "Y" and tem3 >= 37:
+    a_count += 1
+
+# 위급상황 판단
+if a_count >= 2:
     print("E")
 else:
     print("N")
