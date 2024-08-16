2dafc3250aa677bd055ba86c26b47a50c702857a
20s_hoon <39319854+98-jeonghoon@users.noreply.github.com>
2024-08-17 04:06:48 +0900

    Title: 선분 위의 점, Time: 268ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240817/선분 위의 점/point-on-the-line-segment.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n, m;
+    static int[] points;
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        points = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            points[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            System.out.println(upperBound(end) - lowerBound(start));
+        }
+    }
+
+    static int lowerBound(int target) {
+        int left = 0, right = n - 1;
+        int minIdx = n;
+        while (left <= right) {
+            int mid = (right + left) / 2;
+            if (points[mid] >= target) {
+                right = mid - 1;
+                minIdx = Math.min(minIdx, mid);
+            }
+            else{
+                left = mid + 1;
+            }
+        }
+        return minIdx;
+    }
+
+    static int upperBound(int target) {
+        int left = 0, right = n - 1;
+        int minIdx = n;
+        while (left <= right) {
+            int mid = (right + left) / 2;
+            if (points[mid] > target) {
+                right = mid - 1;
+                minIdx = Math.min(minIdx, mid);
+            } else {
+                left = mid + 1;
+            }
+        }
+        return minIdx;
+    }
+}
