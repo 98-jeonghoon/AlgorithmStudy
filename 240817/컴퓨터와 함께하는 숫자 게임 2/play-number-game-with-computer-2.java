6cfe9491ebf98e7a510f1462c50ba24ab0f649d8
20s_hoon <39319854+98-jeonghoon@users.noreply.github.com>
2024-08-17 05:59:10 +0900

    Title: 컴퓨터와 함께하는 숫자 게임 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240817/컴퓨터와 함께하는 숫자 게임 2/play-number-game-with-computer-2.java
+++ 240817/컴퓨터와 함께하는 숫자 게임 2/play-number-game-with-computer-2.java
@@ -4,8 +4,9 @@ import java.util.StringTokenizer;
 
 public class Main {
     static int m;
-    static int a, b;
+    static long a, b;
     static int[] arr;
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         m = Integer.parseInt(br.readLine());
@@ -16,34 +17,33 @@ public class Main {
         }
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        a = Integer.parseInt(st.nextToken());
-        b = Integer.parseInt(st.nextToken());
+        a = Long.parseLong(st.nextToken());
+        b = Long.parseLong(st.nextToken());
 
         int maxValue = Integer.MIN_VALUE;
         int minValue = Integer.MAX_VALUE;
-        for(int i = a; i <= b; i++){
-            int value = seach(i);
+        for(long i = a; i <= b; i++){
+            int value = search((int)i);
             maxValue = Math.max(maxValue, value);
             minValue = Math.min(minValue, value);
         }
 
         System.out.println(minValue + " " + maxValue);
-
     }
 
-    static int seach(int target){
+    static int search(int target){
         int left = 0, right = m - 1;
         int count = 0;
         while(left <= right){
             count++;
-            int mid =(left + right) / 2;
+            int mid = (left + right) / 2;
             if(arr[mid] == target){
                 break;
             }
 
             if(arr[mid] > target){
                 right = mid - 1;
-            }else{
+            } else {
                 left = mid + 1;
             }
         }
