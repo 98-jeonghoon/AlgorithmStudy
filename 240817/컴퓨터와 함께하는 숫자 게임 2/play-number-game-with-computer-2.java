d460c6c6cff0d2ffad5998325ada06df7b0087c3
20s_hoon <39319854+98-jeonghoon@users.noreply.github.com>
2024-08-17 05:55:43 +0900

    Title: 컴퓨터와 함께하는 숫자 게임 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240817/컴퓨터와 함께하는 숫자 게임 2/play-number-game-with-computer-2.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int m;
+    static int a, b;
+    static int[] arr;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        m = Integer.parseInt(br.readLine());
+
+        arr = new int[m];
+        for(int i = 0; i < m; i++){
+            arr[i] = i + 1;
+        }
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        a = Integer.parseInt(st.nextToken());
+        b = Integer.parseInt(st.nextToken());
+
+        int maxValue = Integer.MIN_VALUE;
+        int minValue = Integer.MAX_VALUE;
+        for(int i = a; i <= b; i++){
+            int value = seach(i);
+            maxValue = Math.max(maxValue, value);
+            minValue = Math.min(minValue, value);
+        }
+
+        System.out.println(minValue + " " + maxValue);
+
+    }
+
+    static int seach(int target){
+        int left = 0, right = m - 1;
+        int count = 0;
+        while(left <= right){
+            count++;
+            int mid =(left + right) / 2;
+            if(arr[mid] == target){
+                break;
+            }
+
+            if(arr[mid] > target){
+                right = mid - 1;
+            }else{
+                left = mid + 1;
+            }
+        }
+        return count;
+    }
+}
