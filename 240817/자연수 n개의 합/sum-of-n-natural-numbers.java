9d81b03e9d7e099ca771e5f6258ba738630cce02
20s_hoon <39319854+98-jeonghoon@users.noreply.github.com>
2024-08-17 22:31:11 +0900

    Title: 자연수 n개의 합, Time: 95ms, Memory: 8MB, Status: Passed - CodeTree

--- 240817/자연수 n개의 합/sum-of-n-natural-numbers.java
+++ 240817/자연수 n개의 합/sum-of-n-natural-numbers.java
@@ -2,19 +2,20 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int s = 2000000000;
+    public static final int MAX_S =2000000000;
+    static long s;
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        s = Integer.parseInt(br.readLine());
+        s = Long.parseLong(br.readLine());
         System.out.println(parametricSeach());
 
     }
 
-    static int parametricSeach(){
-        int left = 1, right = s;
-        int maxNum = 0;
+    static long parametricSeach(){
+        long left = 1, right = MAX_S;
+        long maxNum = 0;
         while(left <= right){
-            int mid = (left + right) / 2;
+            long mid = (left + right) / 2;
             // 1부터 n까지 합이 s보다 같거나 작다면
             if(mid * (mid + 1) / 2 <= s ){
                 left = mid + 1;
