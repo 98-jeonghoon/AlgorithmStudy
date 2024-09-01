fa095f4f531b3619b05f7aa08b12029bd7b0eb27
20s_hoon <39319854+98-jeonghoon@users.noreply.github.com>
2024-09-01 22:30:00 +0900

    Title: 범위 내에 있는 점의 수 2, Time: 1694ms, Memory: 49MB, Status: Passed - CodeTree

--- 240901/범위 내에 있는 점의 수 2/the-number-of-points-within-the-range-2.java
+++ 240901/범위 내에 있는 점의 수 2/the-number-of-points-within-the-range-2.java
@@ -2,47 +2,48 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static int SIZE = 1000001;
+    static int n, q;
+    static int[] arr, prefixSum;
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        
-        int n = Integer.parseInt(st.nextToken());
-        int q = Integer.parseInt(st.nextToken());
-        
-        int[] points = new int[n];
+        n = Integer.parseInt(st.nextToken());
+        q = Integer.parseInt(st.nextToken());
+
+        arr = new int[SIZE];
+        prefixSum = new int[SIZE];
+
         st = new StringTokenizer(br.readLine());
-        int maxPoint = 0;
-        for (int i = 0; i < n; i++) {
-            points[i] = Integer.parseInt(st.nextToken());
-            maxPoint = Math.max(maxPoint, points[i]);
+        for (int i = 1; i <= n; i++) {
+            int point = Integer.parseInt(st.nextToken());
+            arr[i] = point;
+            prefixSum[arr[i]]++;
         }
-        
-        int[] prefixSum = new int[maxPoint + 2];
-        
-        for (int point : points) {
-            prefixSum[point]++;
-        }
-        
-        for (int i = 1; i <= maxPoint; i++) {
+
+        prefixSum[0] = arr[0];
+        for (int i = 1; i < SIZE; i++) {
             prefixSum[i] += prefixSum[i - 1];
         }
-        
-        StringBuilder sb = new StringBuilder();
-        
+
         for (int i = 0; i < q; i++) {
             st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            
-            if (a > maxPoint) {
-                sb.append(0).append("\n");
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            int answer = prefixCalc(s, e);
+            if (answer > 0) {
+                System.out.println(answer);
             } else {
-                b = Math.min(b, maxPoint);
-                int result = prefixSum[b] - (a > 0 ? prefixSum[a - 1] : 0);
-                sb.append(result).append("\n");
+                System.out.println(0);
             }
         }
-        
-        System.out.print(sb.toString());
+
+    }
+
+    static int prefixCalc(int s, int e) {
+        if (s == 0){
+            return prefixSum[e];
+        }
+        return prefixSum[e] - prefixSum[s - 1];
     }
 }
