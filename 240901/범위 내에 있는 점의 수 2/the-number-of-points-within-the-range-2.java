a7f9100fb0fcba0773fee5516587a06e4b5b6dc5
20s_hoon <39319854+98-jeonghoon@users.noreply.github.com>
2024-09-01 22:14:09 +0900

    Title: 범위 내에 있는 점의 수 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240901/범위 내에 있는 점의 수 2/the-number-of-points-within-the-range-2.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int SIZE = 1000001;
+    static int n, q;
+    static int[] arr, prefixSum;
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        q = Integer.parseInt(st.nextToken());
+
+        arr = new int[SIZE];
+        prefixSum = new int[SIZE];
+
+        st = new StringTokenizer(br.readLine());
+        int maxValue = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            int point = Integer.parseInt(st.nextToken());
+            arr[point] = 1;
+            maxValue = Math.max(maxValue, point);
+        }
+
+        prefixSum[0] = 0;
+        for (int i = 1; i <= maxValue; i++) {
+            prefixSum[i] = prefixSum[i - 1] + arr[i];
+        }
+
+        for (int i = 0; i < q; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            int answer = prefixCalc(s, e);
+            if (answer > 0) {
+                System.out.println(answer);
+            } else {
+                System.out.println(0);
+            }
+        }
+
+    }
+
+    static int prefixCalc(int s, int e) {
+        return prefixSum[e] - prefixSum[s - 1];
+    }
+}
