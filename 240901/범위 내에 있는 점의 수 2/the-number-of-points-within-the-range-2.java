d3b249eea7b4f0235ad3b3d90c59d7cc60822058
20s_hoon <39319854+98-jeonghoon@users.noreply.github.com>
2024-09-01 22:16:38 +0900

    Title: 범위 내에 있는 점의 수 2, Time: 380ms, Memory: 30MB, Status: Wrong Answer - CodeTree

--- 240901/범위 내에 있는 점의 수 2/the-number-of-points-within-the-range-2.java
+++ 240901/범위 내에 있는 점의 수 2/the-number-of-points-within-the-range-2.java
@@ -22,7 +22,7 @@ public class Main {
             maxValue = Math.max(maxValue, point);
         }
 
-        prefixSum[0] = 0;
+        prefixSum[0] = arr[0];
         for (int i = 1; i <= maxValue; i++) {
             prefixSum[i] = prefixSum[i - 1] + arr[i];
         }
@@ -42,6 +42,9 @@ public class Main {
     }
 
     static int prefixCalc(int s, int e) {
+        if (s == 0){
+            return prefixSum[e];
+        }
         return prefixSum[e] - prefixSum[s - 1];
     }
 }
