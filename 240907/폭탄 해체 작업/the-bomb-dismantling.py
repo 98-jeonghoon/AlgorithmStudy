86cd43464751657737c7b5e00aca2718922ca94a
20s_hoon <39319854+98-jeonghoon@users.noreply.github.com>
2024-09-07 06:08:06 +0900

    Title: 폭탄 해체 작업, Time: 186ms, Memory: 25MB, Status: Passed - CodeTree

--- 240907/폭탄 해체 작업/the-bomb-dismantling.py
+++ 240907/폭탄 해체 작업/the-bomb-dismantling.py
@@ -5,24 +5,24 @@ bomb = []
 
 for _ in range(n):
     score, time = map(int, input().split())
-    bomb.append((time, score))
+    bomb.append((score, time))
 
-# 시간을 기준으로 오름차순 정렬
-bomb.sort()
+# 점수를 기준으로 내림차순 정렬
+bomb.sort(key=lambda x: (-x[0], x[1]))
 
-# 최대 힙을 사용하여 해체할 폭탄 점수를 관리
-max_heap = []
+# 시간을 추적하며 사용할 리스트
+time_slots = [0] * 10001  # 최대 10,000까지의 시간을 다룰 수 있음
+
 total_score = 0
 
-for time, score in bomb:
-    # 우선 해체할 폭탄의 점수를 최대 힙에 추가
-    heapq.heappush(max_heap, -score)
-    
-    # 시간이 초과되지 않도록 관리 (해체할 수 있는 시간 내에서만 처리)
-    if len(max_heap) > time:
-        heapq.heappop(max_heap)  # 점수가 가장 낮은 폭탄은 해체하지 않음
-
-# 남아 있는 폭탄 점수 합산
-total_score = -sum(max_heap)
+# 각 폭탄을 점수가 높은 순서대로 처리
+for score, time in bomb:
+    # 해당 폭탄을 가장 늦게 처리할 수 있는 시간을 찾는다.
+    while time > 0 and time_slots[time] != 0:
+        time -= 1
+    # 남은 시간이 있으면 그 시간대에 폭탄을 처리한다.
+    if time > 0:
+        time_slots[time] = 1
+        total_score += score
 
 print(total_score)
