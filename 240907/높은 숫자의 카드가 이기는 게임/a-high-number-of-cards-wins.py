fc375fd852e6c79b423352d734c2343f929a1751
20s_hoon <39319854+98-jeonghoon@users.noreply.github.com>
2024-09-07 05:24:40 +0900

    Title: 높은 숫자의 카드가 이기는 게임, Time: 202ms, Memory: 32MB, Status: Passed - CodeTree

--- 240907/높은 숫자의 카드가 이기는 게임/a-high-number-of-cards-wins.py
+++ 240907/높은 숫자의 카드가 이기는 게임/a-high-number-of-cards-wins.py
@@ -7,7 +7,7 @@ for _ in range(n):
     value = int(input())
     B.append(value)
 
-# B를 set으로 변환하여 탐색 속도 개선
+# B를 set으로 변환하여 빠르게 탐색
 B_set = set(B)
 
 # A의 카드 리스트 구하기 (B에 없는 카드들)
@@ -26,10 +26,10 @@ answer = 0
 # 두 포인터로 카드 비교
 while A_pointer < n and B_pointer < n:
     if A[A_pointer] > B[B_pointer]:
-        # A의 카드가 B의 카드보다 크면 A가 이김
         answer += 1
+        A_pointer += 1
         B_pointer += 1
-    # A의 카드가 B의 카드보다 작거나 같은 경우에도 A 카드는 소모됨
-    A_pointer += 1
+    else:
+        A_pointer += 1
 
 print(answer)
