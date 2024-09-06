2c2010086458b3a7927a0d293d9cdfb816dff8de
20s_hoon <39319854+98-jeonghoon@users.noreply.github.com>
2024-09-07 06:06:02 +0900

    Title: 폭탄 해체 작업, Time: 116ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240907/폭탄 해체 작업/the-bomb-dismantling.py
+++ 240907/폭탄 해체 작업/the-bomb-dismantling.py
@@ -1,18 +1,28 @@
+import heapq
+
 n = int(input())
 bomb = []
 
 for _ in range(n):
     score, time = map(int, input().split())
-    bomb.append((score, time))
+    bomb.append((time, score))
 
-bomb.sort(key = lambda x : (x[1], -x[0]))
+# 시간을 기준으로 오름차순 정렬
+bomb.sort()
 
-last_time = 0
+# 최대 힙을 사용하여 해체할 폭탄 점수를 관리
+max_heap = []
+total_score = 0
 
-answer = 0
-for score, time in bomb:
-    if time > last_time:
-        last_time = time
-        answer += score
+for time, score in bomb:
+    # 우선 해체할 폭탄의 점수를 최대 힙에 추가
+    heapq.heappush(max_heap, -score)
+    
+    # 시간이 초과되지 않도록 관리 (해체할 수 있는 시간 내에서만 처리)
+    if len(max_heap) > time:
+        heapq.heappop(max_heap)  # 점수가 가장 낮은 폭탄은 해체하지 않음
 
-print(answer)
+# 남아 있는 폭탄 점수 합산
+total_score = -sum(max_heap)
+
+print(total_score)
