3936ee9f83f8d421af7ac600da44f37a59c28192
20s_hoon <39319854+98-jeonghoon@users.noreply.github.com>
2024-09-12 07:20:13 +0900

    Title: 괄호 쌍 만들어주기, Time: 136ms, Memory: 29MB, Status: Passed - CodeTree

--- 240912/괄호 쌍 만들어주기/pair-parentheses.py
+++ 240912/괄호 쌍 만들어주기/pair-parentheses.py
@@ -1,14 +1,25 @@
 s = list(input())
 
-open_cnt = 0
-close_cnt = 0
+open_pair = []
+close_pair = []
 
+# 여는 괄호 쌍을 찾아서 저장
 for i in range(len(s) - 1):
     if s[i] == "(" and s[i + 1] == "(":
-        open_cnt += 1
+        open_pair.append(i)
 
-for i in range(len(s) - 1, 0, -1):
-    if s[i] == ")" and s[i - 1] ==")":
-        close_cnt += 1
+# 닫는 괄호 쌍을 찾아서 저장
+for i in range(len(s) - 1):
+    if s[i] == ")" and s[i + 1] == ")":
+        close_pair.append(i)
 
-print(open_cnt * close_cnt)
+count = 0
+j = 0
+
+# 매칭 가능한 쌍을 계산
+for i in open_pair:
+    while j < len(close_pair) and close_pair[j] <= i:
+        j += 1
+    count += len(close_pair) - j
+
+print(count)
