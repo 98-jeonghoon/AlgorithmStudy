882cbb6ab2dc16582b67e722a5492ff60bef5e0c
20s_hoon <39319854+98-jeonghoon@users.noreply.github.com>
2024-09-17 10:15:00 +0900

    Title: 적석과 흑석 2, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240917/적석과 흑석 2/red-stone-and-black-stone-2.py
+++ 240917/적석과 흑석 2/red-stone-and-black-stone-2.py
@@ -1,26 +1,33 @@
 C, N = map(int, input().split())
 
+
 red_stones = []
 for _ in range(C):
     red_stones.append(int(input()))
 
+
 black_stones = []
 for _ in range(N):
-    a, b = map(int, input().split())
-    black_stones.append((a, b))
+    A, B = map(int, input().split())
+    black_stones.append((A, B))
 
 red_stones.sort()
-black_stones.sort(key=lambda x: x[1])
+black_stones.sort(key=lambda x: x[1])  
 
-count = 0
-j = 0
+# 투 포인터 방식으로 매칭
+i, j = 0, 0  # i는 빨간 돌 인덱스, j는 검정 돌 인덱스
+matches = 0
+used_reds = set()  # 이미 매칭된 빨간 돌들을 추적
 
-for i in range(C):
-    while j < N and black_stones[j][1] < red_stones[i]:
-        j += 1
-    
-    if j < N and black_stones[j][0] <= red_stones[i] <= black_stones[j][1]:
-        count += 1
-        j += 1
+while j < N:
+    # 가능한 빨간 돌을 찾는 과정
+    while i < C and (red_stones[i] < black_stones[j][0] or i in used_reds):
+        i += 1
+    # 매칭이 가능하면 매칭 수행
+    if i < C and black_stones[j][0] <= red_stones[i] <= black_stones[j][1]:
+        matches += 1
+        used_reds.add(i)
+        i += 1
+    j += 1
 
-print(count)
+print(matches)
