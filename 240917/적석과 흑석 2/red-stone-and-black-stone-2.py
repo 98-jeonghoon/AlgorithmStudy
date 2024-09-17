7ac5bf1c5b21968e60d80aae21ef9b820b727da1
20s_hoon <39319854+98-jeonghoon@users.noreply.github.com>
2024-09-17 10:16:54 +0900

    Title: 적석과 흑석 2, Time: 1981ms, Memory: 38MB, Status: Passed - CodeTree

--- 240917/적석과 흑석 2/red-stone-and-black-stone-2.py
+++ 240917/적석과 흑석 2/red-stone-and-black-stone-2.py
@@ -1,33 +1,20 @@
-C, N = map(int, input().split())
+from collections import deque
+from bisect import bisect_left
 
+n, m = map(int, input().split())
+red = [int(input()) for _ in range(n)]
+black = [list(map(int, input().split())) for _ in range(m)]
 
-red_stones = []
-for _ in range(C):
-    red_stones.append(int(input()))
+red.sort()
+black.sort(key=lambda x: (x[1], -x[0]))
 
+black = deque(black)
+result = 0
+while black and red:
+    start, end, = black.popleft()
+    left = bisect_left(red, start)
+    if left < len(red) and start <= red[left] <= end:
+        red.pop(left)
+        result += 1
 
-black_stones = []
-for _ in range(N):
-    A, B = map(int, input().split())
-    black_stones.append((A, B))
-
-red_stones.sort()
-black_stones.sort(key=lambda x: x[1])  
-
-# 투 포인터 방식으로 매칭
-i, j = 0, 0  # i는 빨간 돌 인덱스, j는 검정 돌 인덱스
-matches = 0
-used_reds = set()  # 이미 매칭된 빨간 돌들을 추적
-
-while j < N:
-    # 가능한 빨간 돌을 찾는 과정
-    while i < C and (red_stones[i] < black_stones[j][0] or i in used_reds):
-        i += 1
-    # 매칭이 가능하면 매칭 수행
-    if i < C and black_stones[j][0] <= red_stones[i] <= black_stones[j][1]:
-        matches += 1
-        used_reds.add(i)
-        i += 1
-    j += 1
-
-print(matches)
+print(result)
