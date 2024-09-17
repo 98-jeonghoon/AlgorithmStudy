3a7e1f24f45764f19d088402f9e969d075348281
20s_hoon <39319854+98-jeonghoon@users.noreply.github.com>
2024-09-17 10:14:28 +0900

    Title: 적석과 흑석 2, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240917/적석과 흑석 2/red-stone-and-black-stone-2.py
+++ 240917/적석과 흑석 2/red-stone-and-black-stone-2.py
@@ -4,29 +4,23 @@ red_stones = []
 for _ in range(C):
     red_stones.append(int(input()))
 
-
 black_stones = []
 for _ in range(N):
-    A, B = map(int, input().split())
-    black_stones.append((A, B))
+    a, b = map(int, input().split())
+    black_stones.append((a, b))
 
 red_stones.sort()
 black_stones.sort(key=lambda x: x[1])
 
-# 투 포인터 방식으로 매칭
-i, j = 0, 0  # i는 빨간 돌 인덱스, j는 검정 돌 인덱스
-matches = 0
-while i < C and j < N:
-    if black_stones[j][0] <= red_stones[i] <= black_stones[j][1]:
-        # 매칭 가능하면 매칭하고 둘 다 인덱스를 증가시킴
-        matches += 1
-        i += 1
+count = 0
+j = 0
+
+for i in range(C):
+    while j < N and black_stones[j][1] < red_stones[i]:
         j += 1
-    elif red_stones[i] < black_stones[j][0]:
-        # 빨간 돌의 값이 검정 돌의 A보다 작으면 빨간 돌 인덱스 증가
-        i += 1
-    else:
-        # 검정 돌의 B보다 빨간 돌의 값이 크면 검정 돌 인덱스 증가
+    
+    if j < N and black_stones[j][0] <= red_stones[i] <= black_stones[j][1]:
+        count += 1
         j += 1
 
-print(matches)
+print(count)
