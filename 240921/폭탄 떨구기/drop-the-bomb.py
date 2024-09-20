163ca0111bf56552edddbded2b0485b00046860a
20s_hoon <39319854+98-jeonghoon@users.noreply.github.com>
2024-09-21 00:38:35 +0900

    Title: 폭탄 떨구기, Time: 166ms, Memory: 26MB, Status: Passed - CodeTree

--- 240921/폭탄 떨구기/drop-the-bomb.py
+++ 240921/폭탄 떨구기/drop-the-bomb.py
@@ -1,36 +1,34 @@
-N, K = map(int, input().split())
-points = [int(input()) for _ in range(N)]
+# 변수 선언 및 입력:
+n, k = tuple(map(int, input().split()))
+pos = [int(input()) for _ in range(n)]
 
-points.sort()
 
-def can_remove_all_with_R(R):
-    bombs_used = 0
-    i = 0
+def is_possible(mid):
+    cnt = 1
+    idx = 0                 # 모든 점을 제거하기 위해 몇개의 폭탄이 필요한지 확인합니다.
+    for i in range(n):
+        if pos[i] - pos[idx] <= 2 * mid:
+            continue
+        else:
+            cnt += 1
+            idx = i
+    
+    return cnt <= k
 
-    while i < N:
-        bombs_used += 1
-        start = points[i]  # 현재 폭탄을 떨어뜨릴 시작점
-        # 이 폭탄으로 R 범위 내에 있는 점들을 모두 제거
-        while i < N and points[i] <= start + 2 * R:
-            i += 1
 
-        # 폭탄을 K번 초과해서 사용하면 False
-        if bombs_used > K:
-            return False
+pos.sort()
 
-    return True
+lo = 0                      # 답이 될 수 있는 가장 작은 숫자 값을 설정합니다.
+hi = 10**9                  # 답이 될 수 있는 가장 큰 숫자 값을 설정합니다.
+ans = 10**9                 # 답을 저장합니다.
 
-left, right = 0, 1000000000
-answer = right
-
-while left <= right:
-    mid = (left + right) // 2
-
-    if can_remove_all_with_R(mid):
-        answer = mid  # 가능한 R 값 중 최소값을 찾는다
-        right = mid - 1
+while lo <= hi:             # [lo, hi]가 유효한 구간이면 계속 수행합니다.
+    mid = (lo + hi) // 2    # 가운데 위치를 선택합니다.
+    if is_possible(mid):    # 결정문제에 대한 답이 Yes라면
+        hi = mid - 1        # 왼쪽에 조건을 만족하는 숫자가 더 있을 가능성 때문에 right를 바꿔줍니다.
+        ans = min(ans, mid) # 답의 후보들 중 최솟값을 계속 갱신해줍니다.
     else:
-        left = mid + 1
+        lo = mid + 1        # 결정문제에 대한 답이 No라면 right를 바꿔줍니다.
 
-
-print(answer)
+# 정답을 출력합니다.
+print(ans)
