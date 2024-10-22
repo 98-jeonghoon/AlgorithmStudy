996a0216812b3ef066a314ad655b8e5782778bc9
20s_hoon <39319854+98-jeonghoon@users.noreply.github.com>
2024-10-23 02:25:03 +0900

    Title: 안전 지대, Time: 137ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 241023/안전 지대/comfort-zone.py
@@ -0,0 +1,41 @@
+from collections import deque
+
+n, m = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(n)]
+# visited = [[False] * m for _ in range(n)]
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def bfs(x, y, k):
+    queue = deque()
+    visited[x][y] = True
+    queue.append((x, y))
+    area = [graph[x][y]]
+
+    while queue:
+        x, y = queue.popleft()
+        for d in range(4):
+            nx = x + dx[d]
+            ny = y + dy[d]
+            if 0 <= nx < n and 0 <= ny < m:
+                if graph[nx][ny] > k and visited[nx][ny] == False:
+                    visited[nx][ny] = True
+                    queue.append((nx, ny))
+                    area.append(graph[nx][ny])
+    return area
+
+answer_k = 0
+answer = 0
+for k in range(1, 101):
+    visited = [[False] * m for _ in range(n)]
+    total_area = []
+    for x in range(n):
+        for y in range(m):
+            if graph[x][y] > k and visited[x][y] == False:
+                total_area.append(bfs(x, y, k))
+    if len(total_area) > answer_k:
+        answer_k = len(total_area)
+        answer = k
+
+print(answer, answer_k)
