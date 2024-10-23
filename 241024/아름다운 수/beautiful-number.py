46f896365e4fe7a3ab2cb9bdc9ae6c089e4ae84d
20s_hoon <39319854+98-jeonghoon@users.noreply.github.com>
2024-10-24 03:27:51 +0900

    Title: 아름다운 수, Time: 127ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 241024/아름다운 수/beautiful-number.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+dp = [0] * (n + 5) 
+dp[0] = 1
+
+for i in range(1, n + 1):
+    if i >= 1:
+        dp[i] += dp[i - 1] 
+    if i >= 2:
+        dp[i] += dp[i - 2] 
+    if i >= 3:
+        dp[i] += dp[i - 3] 
+    if i >= 4:
+        dp[i] += dp[i - 4]
+
+print(dp[n])
